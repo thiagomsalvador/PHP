4ff7c51f99b1bf26995c62d343b7eefa13e07b69
THIAGO MANOEL SALVADOR <thiago.msalvador@senacsp.edu.br>
2023-06-26 21:51:13 -0300

    Hello World

---
+++ aula01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World - PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo 'Hello World SUMEMO';
+        ?>
+    </h1>
+</body>
+</html>
